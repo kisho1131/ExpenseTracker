641605eaa4abfc47eb72a1453e511d820f286bad
kisho1131 <sumitkishore0131@gmail.com>
2021-07-25 00:23:28 +0530

    Email Validation

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django import views
-from .views import RegistrationView, UsernameValidationView
+from .views import EmailValidationView, RegistrationView, UsernameValidationView
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -8,4 +8,5 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('register', RegistrationView.as_view(), name = "register" ),
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name = "validate-username"),
+    path('validate-email', csrf_exempt(EmailValidationView.as_view()), name = "validate-email"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,10 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+# from django.core.validators import validate_email
+from validate_email import validate_email
+
+
 # Create your views here.
 
 class RegistrationView(View):
@@ -14,8 +18,20 @@ class UsernameValidationView(View):
         data = json.loads(requests.body)
         username = data['username']
         if not str(username).isalnum():
-            return JsonResponse({'username_error' : 'username should only contain alphanumeric characters'}, status = 400)
+            return JsonResponse({'username_error' : 'Username should only contain Alphanumeric [(a-z)(A-Z)(0-9)] Characters'}, status = 400)
         if User.objects.filter(username=username).exists():
-            return JsonResponse({'username_error' : 'Sorry!! username is Already Taken Choose Another'}, status = 409)
+            return JsonResponse({'username_error' : 'Sorry!! username is Already Taken | Choose Another'}, status = 409)
 
         return JsonResponse({'username_valid':True})
+
+# Email Validation 
+class EmailValidationView(View):
+    def post(self, requests):
+        data = json.loads(requests.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error' : 'Enter Valid Email ID'}, status = 400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error' : 'Sorry!! Email is in Use'}, status = 409)
+
+        return JsonResponse({'email_valid':True})
