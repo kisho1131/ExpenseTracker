48ada24dac864af0bff83cf398e6add927dfcf98
kisho1131 <sumitkishore0131@gmail.com>
2021-07-24 22:36:02 +0530

    adding User registration

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,11 @@
 from django import views
-from .views import RegistrationView
+from .views import RegistrationView, UsernameValidationView
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('register', RegistrationView.as_view(), name = "register" ),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name = "validate-username"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 class RegistrationView(View):
     def get(self, requerts):
         return render(requerts, 'authentication/register.html')
 
+class UsernameValidationView(View):
+    def post(self, requests):
+        data = json.loads(requests.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error' : 'username should only contain alphanumeric characters'}, status = 400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error' : 'Sorry!! username is Already Taken Choose Another'}, status = 409)
+
+        return JsonResponse({'username_valid':True})
