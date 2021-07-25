24dd8bd56afa1e4ee74ffb6a334d504e813ae600
kisho1131 <sumitkishore0131@gmail.com>
2021-07-25 11:32:39 +0530

    Email Validation

--- ExpenseTracker/static/js/register.js
+++ ExpenseTracker/static/js/register.js
@@ -1,12 +1,29 @@
 const usernameField = document.querySelector('#username')
 const feedBackArea = document.querySelector('.invalid_feedback')
 const emailFeedBackArea = document.querySelector('.invalid_emailFeedBack')
+const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput")
+const emailField = document.querySelector('#email')
+const emailSuccessOutput = document.querySelector('.emailSuccessOutput')
+const passwordField = document.querySelector("#password")
+const showPasswordToggle = document.querySelector(".showPasswordToggle")
+
+const handleToggleInput = (e)=>{
+    if(showPasswordToggle.textContent === "SHOW PASSWORD"){
+        passwordField.setAttribute("type", "text")
+        showPasswordToggle.textContent = "HIDE PASSWORD"
+    }else{
+        showPasswordToggle.textContent = "SHOW PASSWORD"
+        passwordField.setAttribute("type", "password")
+    }
+}
+showPasswordToggle.addEventListener('click', handleToggleInput)
 
 // Event Listner for Validating the Email ID 
-const emailField = document.querySelector('#email')
-
 emailField.addEventListener("keyup", (e) =>{
     const emailVal = e.target.value
+    emailSuccessOutput.style.display = "block"
+    emailSuccessOutput.textContent = `Checking ${emailVal}`
+
     emailField.classList.remove("is-invalid");
     emailFeedBackArea.style.display = "none";
     if(emailVal.length > 0){
@@ -16,6 +33,7 @@ emailField.addEventListener("keyup", (e) =>{
                 method : "POST",
         }).then(res => res.json()).then((data)=>{
             console.log('data', data)
+            emailSuccessOutput.style.display = "none"
             if(data.email_error){
                 emailField.classList.add("is-invalid")
                 emailFeedBackArea.style.display = "block"
@@ -29,6 +47,9 @@ emailField.addEventListener("keyup", (e) =>{
 // Event Listener for Validiting the User name 
 usernameField.addEventListener("keyup", (e) => {
     const usernameVal = e.target.value
+    usernameSuccessOutput.style.display = "block"
+    usernameSuccessOutput.textContent = `Checking ${usernameVal}`
+
     usernameField.classList.remove("is-invalid");
     feedBackArea.style.display = "none";
     if(usernameVal.length > 0){
@@ -38,6 +59,7 @@ usernameField.addEventListener("keyup", (e) => {
                 method : "POST",
         }).then(res => res.json()).then((data)=>{
             console.log('data', data)
+            usernameSuccessOutput.style.display = "none"
             if(data.username_error){
                 usernameField.classList.add("is-invalid")
                 feedBackArea.style.display = "block"
